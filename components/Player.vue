f8d2840e4151d50cb7a848aa8eb0d83fa4534b06
JiangShuuu <globelex65@gmail.com>
2022-03-17 17:04:01 +0800

    voice

--- components/Player.vue
+++ components/Player.vue
@@ -4,36 +4,38 @@
       <source :src="musicUrl" type="audio/mpeg">
       Your browser does not support the audio element.
     </audio>
-    <div class="cursor-pointer" @click="play">
-      播放
+    <div class="mt-4">
+      <button class="p-1 border-blue-700 border-2" :class="{'cantstyle': cantplay }" @click="play">
+        播放
+      </button>
+      <button class="p-1 border-blue-700 border-2" @click="pause">
+        暫停
+      </button>
+      <button class="p-1 border-blue-700 border-2" @click="fastFifteen">
+        快進15s
+      </button>
+      <button class="p-1 border-blue-700 border-2" @click="backFifteen">
+        倒退15s
+      </button>
+      <button class="p-1 border-blue-700 border-2" @click="prevMusic">
+        上一首
+      </button>
+      <button class="p-1 border-blue-700 border-2" @click="nextMusic">
+        下一首
+      </button>
+      <ul class="flex mt-2">
+        <p>速度</p>
+        <button class="p-1 ml-4 border-blue-700 border-2" @click="$refs.audio.playbackRate = 0.5">
+          0.5
+        </button>
+        <button class="p-1 ml-2 border-blue-700 border-2" @click="$refs.audio.playbackRate = 1">
+          1
+        </button>
+        <button class="p-1 ml-2 border-blue-700 border-2" @click="$refs.audio.playbackRate = 2">
+          2
+        </button>
+      </ul>
     </div>
-    <div class="cursor-pointer" @click="pause">
-      暫停
-    </div>
-    <div class="cursor-pointer" @click="fastFifteen">
-      快進15s
-    </div>
-    <div class="cursor-pointer" @click="backFifteen">
-      倒退15s
-    </div>
-    <div class="cursor-pointer" @click="prevMusic">
-      上一首
-    </div>
-    <div class="cursor-pointer" @click="nextMusic">
-      下一首
-    </div>
-    <ul class="flex">
-      <p>速度</p>
-      <li class="ml-2 cursor-pointer" @click="$refs.audio.playbackRate = 0.5">
-        0.5
-      </li>
-      <li class="ml-2 cursor-pointer" @click="$refs.audio.playbackRate = 1">
-        1
-      </li>
-      <li class="ml-2 cursor-pointer" @click="$refs.audio.playbackRate = 2">
-        2
-      </li>
-    </ul>
     <div>
       <input
         id="volume"
@@ -41,18 +43,19 @@
         name="volume"
         :value="audioVolume"
         min="0"
-        max="100"
-        step="1"
+        max="1"
+        step="0.01"
         class="cursor-pointer"
         :disabled="muteState"
         @input="voice"
       >
       <label for="volume">volume</label>
     </div>
+    {{ currentTime }}
     <vue-slider v-model="value" @change="procssBar" />
-    <div class="cursor-pointer" @click="muted">
+    <button class="p-1 border-blue-700 border-2" @click="muted">
       靜音
-    </div>
+    </button>
     <div id="panel" class="border-2 mt-4" />
   </div>
 </template>
@@ -69,41 +72,50 @@ export default {
   },
   data () {
     return {
-      audioVolume: null,
+      audioVolume: 0,
       muteState: false,
       music: [
         'https://static.dazedbear.pro/2018-ithome/Swing_Theory.mp3',
         'https://static.dazedbear.pro/2018-ithome/Sinking_Ship.mp3',
-        'https://static.dazedbear.pro/2018-ithome/It_s_All_Happening.mp3'
+        'https://static.dazedbear.pro/2018-ithome/It_s_All_Happening.mp3',
+        '12321321'
       ],
       musicIndex: 0,
       musicUrl: 'https://static.dazedbear.pro/2018-ithome/Swing_Theory.mp3',
       duration: null,
       value: 0,
-      audio: ''
+      currentTime: '',
+      cantplay: false
     }
   },
   mounted () {
     // this.song = new Audio('https://static.dazedbear.pro/2018-ithome/Swing_Theory.mp3')
     // this.song.loop = true
-    this.audioVolume = this.$refs.audio.volume * 100
-    this.$refs.audio.ontimeupdate = () => {
-      this.audioInfo()
-    }
-    // const player = window.document.getElementById('player')
-    // player.ontimeupdate(() => {
-    //   console.log('a')
-    // })
+    this.audioVolume = this.$refs.audio.volume
+    this.update()
   },
   methods: {
+    // time () {
+    //   const currentTime = this.$refs.audio.currentTime
+    //   this.current
+    // },
+    update () {
+      this.$refs.audio.ontimeupdate = () => {
+        this.audioInfo()
+        this.procssBarUpdate()
+      }
+      this.$refs.audio.oncanplay = () => {
+        this.cantplay = false
+        this.$refs.audio.play()
+      }
+    },
     voice (e) {
       console.log(e.target.value)
-      const volume = e.target.value / 100
+      const volume = e.target.value
       this.$refs.audio.volume = volume
     },
     play () {
       this.$refs.audio.play()
-
       console.log(this.$refs.audio.currentTime)
     },
     pause () {
@@ -123,30 +135,39 @@ export default {
       this.muteState = !this.muteState
     },
     nextMusic () {
+      this.$refs.audio.pause()
+      this.value = 0
       this.musicIndex++
       if (this.musicIndex > this.music.length - 1) {
         this.musicIndex = 0
       }
       this.musicUrl = this.music[this.musicIndex]
       this.$refs.audio.load()
-      this.$refs.audio.play()
+      this.cantplay = true
     },
     prevMusic () {
+      this.$refs.audio.pause()
       this.musicIndex--
       if (this.musicIndex < 0) {
         this.musicIndex = this.music.length - 1
       }
       this.musicUrl = this.music[this.musicIndex]
       this.$refs.audio.load()
-      this.$refs.audio.play()
     },
-    procssBar (e) {
-      console.log(e)
-      console.log('hi')
+    // 進度條更新
+    procssBarUpdate () {
+      const currentTime = this.$refs.audio.currentTime
       const duration = this.$refs.audio.duration
-      const total = this.$refs.audio.currentTime
-      console.log('duration', duration)
-      console.log('total', total)
+      console.log(duration, currentTime)
+      if (Number.isFinite(duration)) {
+        const percentage = currentTime / duration * 100
+        this.value = percentage
+      }
+    },
+    // 進度條拖曳
+    procssBar (e) {
+      const duration = this.$refs.audio.duration
+      this.$refs.audio.currentTime = e / 100 * duration
     },
     audioInfo () {
       const player = this.$refs.audio
@@ -160,3 +181,9 @@ export default {
   }
 }
 </script>
+<style scoped>
+.cantstyle {
+  background-color: gray;
+  color: white
+}
+</style>
